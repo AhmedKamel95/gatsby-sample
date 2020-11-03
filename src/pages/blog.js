e9865cb0320e30fb95daaa15f6acb59c74f4e5b3
Ahmed Hanafy <ahmed.hanafy@va.ai>
2020-11-03 14:12:17 +0200

    Second Commit

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
-import "../styles/index.scss"
+import blogStyles from "./blog.module.scss"
 
 const BlogPage = () => {
   const {
@@ -28,9 +28,9 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <ol className={blogStyles.posts}>
         {edges.map((edge, index) => (
-          <li key={index}>
+          <li key={index} className={blogStyles.post}>
             <Link to={`/blog/${edge.node.fields.slug}`}>
               <h2>{edge.node.frontmatter.title}</h2>
               <p>{edge.node.frontmatter.date}</p>
